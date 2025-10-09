a219d30fb7fd2ae163003eb37f4e7c6e8268bfc0
Rahul Kumar <rahulkumarshukla4627@gmail.com>
2025-10-09 06:49:39 +0530

    changes

--- src/borrowed-books/borrowed-books.controller.ts
+++ src/borrowed-books/borrowed-books.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { BorrowedBooksService } from './borrowed-books.service';
+import { BorrowedBookDto, ReturnBookDto } from './borrowed-books.dto';
 
 @Controller('borrowed-books')
-export class BorrowedBooksController {}
+export class BorrowedBooksController {
+    constructor(private readonly borrowedBooksService: BorrowedBooksService){}
+
+    @Post('borrow')
+    borrowBook(@Body() borrowBookDto: BorrowedBookDto){
+        return this.borrowedBooksService.borrowBook(borrowBookDto);
+    }
+
+    @Post('return')
+    returnBook(@Body() returnBookDto: ReturnBookDto){
+        return this.borrowedBooksService.returnBook(returnBookDto);
+    }
+
+    @Get("user/:userId")
+    findAllforUser(@Param('userId') userId: string){
+        return this.borrowedBooksService.findAllForUser(userId);
+    }
+    
+    @Get()
+    findAll(){
+        return this.borrowedBooksService.findALl();
+    }
+
+    @Get(":id")
+    findone(@Param('id') id: string){
+        return this.borrowedBooksService.findOne(id);
+    }
+
+
+}
--- src/borrowed-books/borrowed-books.module.ts
+++ src/borrowed-books/borrowed-books.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { BorrowedBooksController } from './borrowed-books.controller';
+import { BorrowedBooksService } from './borrowed-books.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-@Module({})
+@Module({
+    controllers: [BorrowedBooksController],
+    providers: [BorrowedBooksService, PrismaService],
+    exports: [BorrowedBooksService],
+})
 export class BorrowedBooksModule {}
